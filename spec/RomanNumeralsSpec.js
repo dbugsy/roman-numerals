8db22891cdab61848f0d29ea3357012fb6e1d8cb
dbugsy <dpbergquist@gmail.com>
2017-10-17 16:12:14 +0100

    Convert all single digit numerals

--- spec/RomanNumeralsSpec.js
+++ spec/RomanNumeralsSpec.js
@@ -1,18 +1,40 @@
 const RomanNumeral = require('../src/RomanNumeral');
 
 describe('RomanNumeral converts numbers to numerals', () => {
-  it('converts 1 to I', () => {
-    const romanNumeral = new RomanNumeral(1);
-    expect(romanNumeral.numeral).toEqual('I');
-  });
+  describe('Conversions involving a single numeral', () => {
+    it('converts 1 to I', () => {
+      const romanNumeral = new RomanNumeral(1);
+      expect(romanNumeral.numeral).toEqual('I');
+    });
 
-  it('converts 5 to V', () => {
-    const romanNumeral = new RomanNumeral(5);
-    expect(romanNumeral.numeral).toEqual('V');
-  });
+    it('converts 5 to V', () => {
+      const romanNumeral = new RomanNumeral(5);
+      expect(romanNumeral.numeral).toEqual('V');
+    });
 
-  it('converts 10 to X', () => {
-    const romanNumeral = new RomanNumeral(10);
-    expect(romanNumeral.numeral).toEqual('X');
+    it('converts 10 to X', () => {
+      const romanNumeral = new RomanNumeral(10);
+      expect(romanNumeral.numeral).toEqual('X');
+    });
+
+    it('converts 50 to L', () => {
+      const romanNumeral = new RomanNumeral(50);
+      expect(romanNumeral.numeral).toEqual('L');
+    });
+
+    it('converts 100 to C', () => {
+      const romanNumeral = new RomanNumeral(100);
+      expect(romanNumeral.numeral).toEqual('C');
+    });
+
+    it('converts 500 to D', () => {
+      const romanNumeral = new RomanNumeral(500);
+      expect(romanNumeral.numeral).toEqual('D');
+    });
+
+    it('converts 1000 to M', () => {
+      const romanNumeral = new RomanNumeral(1000);
+      expect(romanNumeral.numeral).toEqual('M');
+    });
   });
 });
